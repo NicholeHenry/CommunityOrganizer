4483ab773ce4e75c8a821fe5d02f5c3c6b5c08f4
Nichole Henry <nichole.henry12@gmail.com>
2019-09-28 19:48:42 -0500

    finishing resident and informationboard added background image

---
+++ CommunityOrganizer/Controllers/InformationBoardController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CommunityOrganizer.Data.Repositories;
+using CommunityOrganizer.ViewModels.InformationBoard;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommunityOrganizer.Controllers
+{
+    public class InformationBoardController : Controller
+    {
+        private Factory repositoryFactory;
+
+        public InformationBoardController(Factory repositoryFactory)
+        {
+            this.repositoryFactory = repositoryFactory;
+        }
+        public IActionResult Index()
+        {
+            List<InformationBoardListViewModel> informationBoards = InformationBoardListViewModel.GetInformationBoard(repositoryFactory);
+            return View(informationBoards);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            InformationBoardCreateViewModel model = new InformationBoardCreateViewModel(repositoryFactory);
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Create(InformationBoardCreateViewModel model)
+        {
+            model.Persist(repositoryFactory);
+            return RedirectToAction(controllerName: nameof(InformationBoardController), actionName: nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            return View(new InformationBoardEditViewModel(id));
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, InformationBoardEditViewModel informationBoard)
+        {
+            repositoryFactory.GetInformationBoardRepository().Delete(id);
+            return RedirectToAction(actionName: nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            repositoryFactory.GetInformationBoardRepository().Delete(id);
+            return RedirectToAction(actionName: nameof(Index));
+        }
+    }
+}
--- CommunityOrganizer/Controllers/ResidentController.cs
+++ CommunityOrganizer/Controllers/ResidentController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CommunityOrganizer.Data.Repositories;
+using CommunityOrganizer.Models;
 using CommunityOrganizer.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,9 +12,9 @@ namespace CommunityOrganizer.Controllers
     public class ResidentController : Controller
     {
 
-        private TaskFactory repositoryFactory;
+        private Factory repositoryFactory;
 
-        public ResidentController(TaskFactory repositoryFactory)
+        public ResidentController(Factory repositoryFactory)
         {
             this.repositoryFactory = repositoryFactory;
         }
@@ -21,5 +23,40 @@ namespace CommunityOrganizer.Controllers
             List<ResidentListViewModel> residents = ResidentListViewModel.GetResident(repositoryFactory);
             return View(residents);
         }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ResidentCreateViewModel model = new ResidentCreateViewModel(repositoryFactory);
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Create(ResidentCreateViewModel model)
+        {
+            model.Persist(repositoryFactory);
+            return RedirectToAction(controllerName: nameof(Resident), actionName: nameof(Index));
+        }
+        
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            return View(new ResidentEditViewModel(id));
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, ResidentEditViewModel resident)
+        {
+            repositoryFactory.GetResidentRepository().Delete(id);
+            return RedirectToAction(actionName: nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            repositoryFactory.GetResidentRepository().Delete(id);
+            return RedirectToAction(actionName: nameof(Index));
+        }
     }
+    
 }
